48e1623aed7ef0dbcbd9dc600339329286b0030e
Shemar Dacosta <dshemar6@gmail.com>
2018-12-03 02:19:53 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,15 @@
+def instantiate_new_array
+  newArray = []
+end
+
+def array_with_two_elements
+  newArray = [6,9]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[0]
+end
